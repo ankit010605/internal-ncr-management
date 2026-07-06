ec57a70f206b471240c336c0f7675febd6178d67
ankit010605 <meankit77137@gmail.com>
2026-07-06 11:49:11 +0530

    Connect frontend to deployed backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,14 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:5000/api",
+// });
+
+// export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://internal-ncr-management.onrender.com/api",
 });
 
 export default api;
